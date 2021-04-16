74417bb6b3da7a18ce5b00af3202fb88c943ce77
Thái Nguyễn <nguyenduythai12@gmail.com>
2021-04-16 09:14:02 +0700

    update

--- src/Services/Dynamic/Dynamic.API/Startup.cs
+++ src/Services/Dynamic/Dynamic.API/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -23,10 +22,7 @@ namespace Dynamic.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
